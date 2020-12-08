823497cf1e2d57551380525c833ff14bd7eb47d8
Jason G <jasondouglasgibbs@gmail.com>
2020-12-08 09:59:23 -0500

    Formatting updates.

--- COVID19_DataPullTableGraph_Builder.R
+++ COVID19_DataPullTableGraph_Builder.R
@@ -13,16 +13,17 @@ library(tidyr)
 library(orca)
 library(processx)
 ##Set working directory for personal laptop##
-setwd("D:\\Users\\fight\\Documents\\COVID19 Code")
+##setwd("D:\\Users\\fight\\Documents\\COVID19 Code")
 
 ##Set working directory for work laptop##
-##setwd("C:\\Users\\jason.d.gibbs1\\Desktop\\COVID-19 R")##
+##setwd("C:\\Users\\jason.d.gibbs1\\Desktop\\COVID-19 R")
 
 ##Set working directory for desktop computer##
-#setwd("C:\\Users\\fight\\Documents\\COVID-19 R File")
+setwd("C:\\Users\\fight\\Documents\\COVID-19 R File")
 
 ##Sets a variable for the working directory for use at the end of the script##
 wd<-getwd()
+
 ##US Confirmed Cases##
 ##Pulls data from Johns Hopkins CSSE GitHub at the below link, turns into a TidyVerse tibble##
 URL_Cases<-"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
